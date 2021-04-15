3f37d92e7e63feed84777da2280a940410d138d2
wongj <j.wong@dkfz-heidelberg.de>
2021-04-15 14:54:02 +0200

    updated

--- R_library/R/sigdriver.R
+++ R_library/R/sigdriver.R
@@ -23,10 +23,9 @@ sigDriver <- function(signature_test,
 	#tweak cut-off for signatures with lower median sample mutation load
 	if (signature_test == "SBS84" || signature_test == "SBS9"){
 		print("lower sample mutation load mode")
-		minentityposcasespct = 2
+		minentityposcasespct = 0.02
 	}
 	
-
 	#run preparation
 	somaticvarranges = read_variants_ranges(variant_file)
 	sigexpinfo = read_signature_exposures_matrix(signature_file)
@@ -35,7 +34,13 @@ sigDriver <- function(signature_test,
 	sampleinfo = filter_sample_info_matrix_by_vrange(sampleinfo=sampleinfo,somaticvarranges=somaticvarranges)
 	entities_include = get_signature_positive_entities(sampleinfo=sampleinfo,minentityposcasespct=minentityposcasespct,maxentityposcasespct=maxentityposcasespct)
 	sampleinfofiltered = filter_sample_info_matrix(sampleinfo=sampleinfo,sigexpinfo=sigexpinfo,entities_include=entities_include)
-	print(head(sampleinfofiltered))
+	
+	#termination criterias
+	if (dim(sampleinfofiltered)[1] == 0){
+		print("no samples left, stop")
+		quit()
+	}
+
 
 	#keep only tumors to be tested in variants table
 	gns = generate_testing_unit_genomic_bins(somaticvarranges)
