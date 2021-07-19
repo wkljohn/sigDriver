5776f153000c3c84425b7f9401e4501ac9a388c3
wongj <j.wong@dkfz-heidelberg.de>
2021-07-19 17:07:51 +0200

    updated

--- R_library/R/association_SKATplus_hotspot_V4.R
+++ R_library/R/association_SKATplus_hotspot_V4.R
@@ -29,7 +29,7 @@ doassocandwriteSKATPLUShotspot <- function (igene,
     }
   
   
-  out <- tryCatch({
+  #out <- tryCatch({
 	  lookuparray = data.frame(cbind(c("chr1","chr2","chr3","chr4","chr5","chr6","chr7","chr8","chr9","chr10","chr11","chr12","chr13","chr14","chr15","chr16","chr17","chr18","chr19","chr20","chr21","chr22","chrX","chrY"),
 	                                 c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24)),stringsAsFactors = FALSE)
 	  rownames(lookuparray) = lookuparray$X1
@@ -293,12 +293,12 @@ doassocandwriteSKATPLUShotspot <- function (igene,
       
     }
    
-  },
-  error=function(cond) {
-    rtnvar=c(genetest,"ERR","ERR","ERR")
-    rtnline=paste(rtnvar,collapse=" ",sep=" ")
-    print(rtnline)
-    write(rtnline,file=outfile,append=TRUE)
-    return(rtnvar)
-  })
+#  },
+#  error=function(cond) {
+#    rtnvar=c(genetest,"ERR","ERR","ERR")
+#    rtnline=paste(rtnvar,collapse=" ",sep=" ")
+#    print(rtnline)
+#    write(rtnline,file=outfile,append=TRUE)
+#    return(rtnvar)
+#  })
 }
