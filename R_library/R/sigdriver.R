2adc6d023c136abdbb444c8cd66e110e08ee9a47
wongj <j.wong@dkfz-heidelberg.de>
2021-08-03 17:13:53 +0200

    updated

--- R_library/R/sigdriver.R
+++ R_library/R/sigdriver.R
@@ -23,9 +23,9 @@ sigDriver <- function(signature_test,
 	#require(sigDriver)
 	
 	#tweak cut-off for signatures with lower median sample mutation load
-	if (signature_test == "SBS84" || signature_test == "SBS9" || signature_test == "SBS3"){
+	if (signature_test == "SBS84X" || signature_test == "SBS9" || signature_test == "SBS3"){
 		print("lower sample mutation load mode")
-		minentityposcasespct = 0.015
+		minentityposcasespct = 0.02 #15
 		##min_testing_bin_vars = 7
 		##frame_pruned_min_nvar = 7
 	}
