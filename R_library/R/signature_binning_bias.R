07dbe18f28e21a5ea318cc1a949fc93649f922d9
wongj <j.wong@dkfz-heidelberg.de>
2021-08-18 13:20:53 +0200

    updated

--- R_library/R/signature_binning_bias.R
+++ R_library/R/signature_binning_bias.R
@@ -21,7 +21,7 @@ signatureRepresentationAdjustment <- function(gns,
                              somaticvarranges,
                              samplemetatablewithentity,
                              threads,
-                             variantFactor=1.5,
+                             variantFactor=1.2,
                              entityFactor=1,
                              binStatsList=NULL){
    print("Calculating signature-bin distribution")
