b1982f195752ec8d296bfc48379a2a711f916f53
wongj <j.wong@dkfz-heidelberg.de>
2021-09-06 11:53:54 +0200

    updated

--- R_library/R/association_SKAT_hotspot_V4Perm.R
+++ R_library/R/association_SKAT_hotspot_V4Perm.R
@@ -27,7 +27,7 @@ doassocandwriteSKAThotspotPerm <- function (igene,
     }
   
   samplemetatablewithentityorg = samplemetatablewithentity
-  out <- tryCatch({
+  #out <- tryCatch({
 	  lookuparray = data.frame(cbind(c("chr1","chr2","chr3","chr4","chr5","chr6","chr7","chr8","chr9","chr10","chr11","chr12","chr13","chr14","chr15","chr16","chr17","chr18","chr19","chr20","chr21","chr22","chrX","chrY"),
 	                                 c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24)),stringsAsFactors = FALSE)
 	  rownames(lookuparray) = lookuparray$X1
@@ -307,12 +307,12 @@ doassocandwriteSKAThotspotPerm <- function (igene,
       return(rtnvar)
       
     }
-  },
-  error=function(cond) {
-    rtnvar=c(genetest,"ERR","ERR","ERR")
-    rtnline=paste(rtnvar,collapse=" ",sep=" ")
-    print(rtnline)
-    write(rtnline,file=outfile,append=TRUE)
-    return(rtnvar)
-  })
+#  },
+#  error=function(cond) {
+#    rtnvar=c(genetest,"ERR","ERR","ERR")
+#    rtnline=paste(rtnvar,collapse=" ",sep=" ")
+#    print(rtnline)
+#    write(rtnline,file=outfile,append=TRUE)
+#    return(rtnvar)
+#  })
 }
