0bcfdcfa5cdddd77ad12a36b5ceb60c1d19ce5e4
wongj <j.wong@dkfz-heidelberg.de>
2021-09-14 13:57:07 +0200

    updated

--- R_library/R/association_SKAT_hotspot_V4Perm.R
+++ R_library/R/association_SKAT_hotspot_V4Perm.R
@@ -27,6 +27,8 @@ doassocandwriteSKAThotspotPerm <- function (igene,
     }
   
   samplemetatablewithentityorg = samplemetatablewithentity
+  weightframencLIST = c()
+  
   #out <- tryCatch({
 	  lookuparray = data.frame(cbind(c("chr1","chr2","chr3","chr4","chr5","chr6","chr7","chr8","chr9","chr10","chr11","chr12","chr13","chr14","chr15","chr16","chr17","chr18","chr19","chr20","chr21","chr22","chrX","chrY"),
 	                                 c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24)),stringsAsFactors = FALSE)
@@ -221,6 +223,7 @@ doassocandwriteSKAThotspotPerm <- function (igene,
 	  
 	  #head(samplemetatablewithentitygt)
 	  if (length(weightframencLIST) == 0){
+	  
 	    rtnvar=c(genetest,skipvarname,testedRegionCoordinates,0,0,dim(samplemetatablewithentity)[1],0,1,ref_pval,paste(0,0,0,0,0,0,0,0,0,0,collapse=" "))
 	    
       rtnline=paste(rtnvar,collapse=" ",sep=" ")
